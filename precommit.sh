a29a6cf96208234751b43bf77f174d88388b9889
Max Campbell <me@0m.ax>
2021-09-14 19:25:48 +0200

    fixed wildcard

--- precommit.sh
+++ precommit.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sort -o domains.txt domains.txt
-cat domains.txt | awk '{print "google.*###islrg a[href*=\"*." $0 "\"]:nth-ancestor(1)\ngoogle.*###search a[href*=\"*." $0 "\"]:nth-ancestor(2)" }' > list.txt
+cat domains.txt | awk '{print "google.*###islrg a[href*=\"" $0 "\"]:nth-ancestor(1)\ngoogle.*###search a[href*=\"" $0 "\"]:nth-ancestor(2)" }' > list.txt
